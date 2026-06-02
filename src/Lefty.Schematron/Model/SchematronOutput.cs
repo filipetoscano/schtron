0f8d615c7d5fa45303bded4099adca42a3ba14e0
Filipe Toscano <filipe.toscano@gmail.com>
2026-06-02 18:47:17 +0100

    Add .IsValid property

--- src/Lefty.Schematron/Model/SchematronOutput.cs
+++ src/Lefty.Schematron/Model/SchematronOutput.cs
@@ -4,5 +4,15 @@
 public class SchematronOutput
 {
     /// <summary />
+    public bool IsValid
+    {
+        get
+        {
+            return this.Lines.Any( x => x is FailedAssert ) == false;
+        }
+    }
+
+
+    /// <summary />
     public required IReadOnlyList<ISchematronLine> Lines { get; init; }
 }
